5acf8a6876b113ed00d722ab4fba11d07166be1d
Muhammad Abdullah <95620935+Abdullah789034@users.noreply.github.com>
2024-03-04 02:37:38 +0500

    script

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-var url;
-
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var currentTab = tabs[0];
-    url = currentTab.url;
-    console.log("Current tab URL:", url);
-    document.getElementById('pg').innerText = url;
-});
-
-
-console.log(url);
-
-
 
 function checkContent(url) {
     if (url.includes('pahe.win')) {
@@ -21,8 +8,34 @@ function checkContent(url) {
 
 
 
-function autoSurf(url) {
-    console.log('ok');
+function autoSurf() {
+    var links = document.querySelectorAll('a.btn.btn-secondary.btn-block.redirect[rel="nofollow"]');
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].innerText.trim() === "Continue") {
+            var link = links[i].href;
+            console.log("Link:", link);
+            break;
+        }
+    }
+    if (!link) {
+        console.error('Link not found');
+    }
+
 }
+
+
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentTab = tabs[0];
+    var url = currentTab.url;
+    console.log("Current tab URL:", url);
+    document.getElementById('pg').innerText = url;
+    if (checkContent(url)) {
+        autoSurf();
+    }
+});
+
+
+
 
 
