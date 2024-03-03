a03ee1ee7e981e670eb69e0c8cd8c6946e290999
Muhammad Abdullah <95620935+Abdullah789034@users.noreply.github.com>
2024-03-04 03:16:10 +0500

    error

--- script.js
+++ script.js
@@ -8,18 +8,34 @@ function checkContent(url) {
 
 
 
-function autoSurf() {
-    var links = document.querySelectorAll('a.btn.btn-secondary.btn-block.redirect[rel="nofollow"]');
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].innerText.trim() === "Continue") {
-            var link = links[i].href;
-            console.log("Link:", link);
-            break;
-        }
-    }
-    if (!link) {
-        console.error('Link not found');
-    }
+function autoSurf(url) {
+    document.getElementById('pg').innerText = "Downloading...";
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            else {
+                return response.text();
+            }
+        })
+        .then(html => {
+            var tempDiv = document.createElement('div');
+            tempDiv.innerHTML = html;
+            var linkElement = tempDiv.querySelector('a[href="https://kwik.si/f/gu2IwYKGDNeZ"].btn.btn-secondary.btn-block.redirect[rel="nofollow"]');
+            if (linkElement) {
+                console.log("Link found:", linkElement.href);
+                // Perform actions with the linkElement here
+            } else {
+                console.error('Link not found');
+            }
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+
+
+
 
 }
 
@@ -31,7 +47,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     console.log("Current tab URL:", url);
     document.getElementById('pg').innerText = url;
     if (checkContent(url)) {
-        autoSurf();
+        autoSurf(url);
     }
 });
 
